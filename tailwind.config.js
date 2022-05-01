360daf9349d61a09c1489de1745ed8c5186b1899
EricQyhan <Ericqyhan@gmail.com>
2022-04-30 22:23:01 -0400

    added tailwinds to project

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    extend: {
+      colors: {
+        "bitcoin-orange": "#FF9900",
+      },
+    },
+  },
+  plugins: [],
+};
